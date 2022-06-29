8ce691fcfaa63072773065842fd0924f781a0de0
root <riturajphukan5@gmail.com>
2022-06-29 15:36:43 +0000

    Added a vnc server setup

---
+++ scripts/installation/vncSetup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install xfce4 xfce4-goodies
+
+sudo apt install tightvncserver
+
+printf 'n\n' | vncserver
+
+vncserver -kill :1
+
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+echo "#!/bin/bash\nxrdb $HOME/.Xresources\nstartxfce4 &" > ~/.vnc/xstartup
+
+chmod +x ~/.vnc/xstartup
+
+vncserver -geometry 1366x768 -localhost
+
+echo "Use the following command to connect"
+echo "\n"
+echo "ssh -L 59000:localhost:5901 -N -f -l root <server-public-ipv4>"
